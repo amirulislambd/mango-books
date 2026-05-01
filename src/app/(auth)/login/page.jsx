bbcfb9067659d4248e38e450d0354db4bd3eee32
amirulislambd <amirulislambd313@gmail.com>
2026-05-02 01:39:02 +0600

    added authantication system of login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,11 @@
 "use client";
 import WelcomeText from "@/components/shared/auth/WelcomeText";
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { FaGoogle } from "react-icons/fa";
-
+import { toast } from "react-toastify";
 
 const LoginPage = () => {
   const {
@@ -12,67 +14,114 @@ const LoginPage = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (info) => {
+    const { email, password } = info;
+
+    try {
+      const { data, error } = await authClient.signIn.email({
+        email: email,
+        password: password,
+        callbackURL: "/",
+        redirect: "/",
+      });
+
+      if (error) {
+        toast.error(error.message, {
+          position: "bottom-center",
+          autoClose: 1500,
+        });
+      }
+
+      if (data) {
+        toast.success("Successfully Registered! Welcome to MangoBooks", {
+          position: "bottom-center",
+          autoClose: 1500,
+        });
+        await authClient.signOut();
+        setTimeout(() => {
+          router.push("/login");
+        }, 1500);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const signWithGoogle = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
   };
 
   return (
     <div class="backdrop-blur-2xl bg-white/5 border border-white/10 rounded-xl shadow-2xl  bg-[radial-gradient(circle_at_20%_30%,#4f46e5_0%,transparent_40%),radial-gradient(circle_at_80%_70%,#7c03d3_0%,transparent_40%)] md:m-10 p-4 md:p-8 lg:p-12">
       <div className=" grid grid-cols-1 lg:grid-cols-2 md:gap-8 items-center justify-center">
-        <div className="p-6 m-   flex-1  flex-col items-center justify-center">         
-        <WelcomeText/>
+        <div className="p-6 m-   flex-1  flex-col items-center justify-center">
+          <WelcomeText />
         </div>
         {/* form */}
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="p-6 max-w-md mx-auto space-y-2 md:space-y-4 lg:space-y-6 bg-white/10 rounded-lg shadow-md w-full"
-        >
-          <div>
-            <h1 className="text-2xl font-bold text-center">Welcome Back</h1>
-            <p className="text-white/80 text-center">
+        <div className="p-6 max-w-md mx-auto space-y-2 md:space-y-4 lg:space-y-6 bg-white/10 rounded-lg shadow-md w-full">
+        <div>
+              <h1 className="text-2xl font-bold text-center">Welcome Back</h1>
+              <p className="text-white/80 text-center">
                 Access your personal archive.
-            </p>
+              </p>
+            </div>
+          <form className="space-y-2 md:space-y-4 " onSubmit={handleSubmit(onSubmit)}>
+           
+            <fieldset>
+              <legend>Email</legend>
+              <input
+                type="email"
+                {...register("email", { required: " Email is required" })}
+                placeholder="Email"
+                className="w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white placeholder:text-white/40 focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 outline-none transition-all"
+              />
+              {errors.email && (
+                <p className="text-red-500">{errors.email.message}</p>
+              )}
+            </fieldset>
+            <fieldset>
+              <legend>Password</legend>
+              <input
+                type="password"
+                {...register("password", { required: "Password is required" })}
+                placeholder="Password"
+                class="w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white placeholder-white/40 focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 outline-none transition-all"
+              />
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
+            </fieldset>
+            <button
+              class="bg-gradient-to-br from-[#4f46e5] to-[#7c03d3] shadow-[0_0_30px_-5px_rgba(79,70,229,0.4)] text-white font-semibold py-3 px-6 rounded-lg hover:scale-[1.02] active:scale-[0.98] transition-transform w-full cursor-pointer  duration-500 "
+              type="submit"
+            >
+              Login
+            </button>
+            <h1 className="font-bold text-center border-b border-indigo-500">
+              Or
+            </h1>
+          </form>
+            <button
+              onClick={signWithGoogle}
+              class="bg-gradient-to-br from-[#4f46e5] to-[#7c03d3] shadow-[0_0_30px_-5px_rgba(79,70,229,0.4)] text-white font-semibold py-3 px-6 rounded-lg hover:scale-[1.02] active:scale-[0.98] transition-transform w-full cursor-pointer duration-500"
+            >
+              <span className="flex gap-2 items-center justify-center">
+                <FaGoogle />
+                Sign in with Google
+              </span>
+            </button>
+          <div className="mt-6 text-xs md:text-sm text-center">
+            <span className="text-gray-500">New to MangoBooks?</span>
+            <Link
+              href="/register"
+              className="ml-2 text-orange-500 hover:text-orange-700 font-bold transition-colors"
+            >
+              Create an Account
+            </Link>
           </div>
-          <fieldset>
-            <legend>Email</legend>
-            <input
-              type="email"
-              {...register("email", { required: " Email is required" })}
-              placeholder="Email"
-              className="w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white placeholder:text-white/40 focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 outline-none transition-all"
-            />
-            {errors.email && (
-              <p className="text-red-500">{errors.email.message}</p>
-            )}
-          </fieldset>
-          <fieldset>
-            <legend>Password</legend>
-            <input
-              type="password"
-              {...register("password", { required: "Password is required" })}
-              placeholder="Password"
-              class="w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white placeholder-white/40 focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 outline-none transition-all"
-            />
-            {errors.password && (
-              <p className="text-red-500">{errors.password.message}</p>
-            )}
-          </fieldset>
-          <button
-            class="bg-gradient-to-br from-[#4f46e5] to-[#7c03d3] shadow-[0_0_30px_-5px_rgba(79,70,229,0.4)] text-white font-semibold py-3 px-6 rounded-lg hover:scale-[1.02] active:scale-[0.98] transition-transform w-full cursor-pointer  duration-500 "
-            type="submit"
-          >
-            Login
-          </button>
-          <h1 className="font-bold text-center border-b border-indigo-500">
-            Or
-          </h1>
-          <button class="bg-gradient-to-br from-[#4f46e5] to-[#7c03d3] shadow-[0_0_30px_-5px_rgba(79,70,229,0.4)] text-white font-semibold py-3 px-6 rounded-lg hover:scale-[1.02] active:scale-[0.98] transition-transform w-full cursor-pointer duration-500">
-            <span className="flex gap-2 items-center justify-center">
-              <FaGoogle />
-              Sign in with Google
-            </span>
-          </button>
-        </form>
+        </div>
       </div>
     </div>
   );
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -3,6 +3,7 @@ import WelcomeText from "@/components/shared/auth/WelcomeText";
 import { authClient } from "@/lib/auth-client";
 import { getImgBbURL } from "@/lib/dataFetch";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { useForm } from "react-hook-form";
@@ -23,7 +24,6 @@ const RegisterPage = () => {
     photoUrl && photoUrl.length > 0 ? URL.createObjectURL(photoUrl[0]) : null;
 
   const onSubmit = async (info) => {
-
     const { name, email, password, photo } = info;
 
     try {
@@ -59,11 +59,11 @@ const RegisterPage = () => {
     }
   };
 
-  const signWithGoogle =async ()=>{
-const data = await authClient.signIn.social({
-  provider:'google'
-})
-  }
+  const signWithGoogle = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+  };
 
   return (
     <div class="backdrop-blur-2xl bg-white/5 border border-white/10 rounded-xl shadow-2xl  bg-[radial-gradient(circle_at_20%_30%,#4f46e5_0%,transparent_40%),radial-gradient(circle_at_80%_70%,#7c03d3_0%,transparent_40%)] md:m-10 p-4 md:p-8 lg:p-12">
@@ -72,10 +72,7 @@ const data = await authClient.signIn.social({
           <WelcomeText />
         </div>
         {/* form */}
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="p-6 max-w-md mx-auto space-y-2 md:space-y-4 lg:space-y-6 bg-white/10 rounded-lg shadow-md w-full relative"
-        >
+        <div className="p-6 max-w-md mx-auto space-y-2 md:space-y-4 lg:space-y-6 bg-white/10 rounded-lg shadow-md w-full relative">
           <div>
             {photo && (
               <div className="absolute top-8 left-4 border border-indigo-500/50 backdrop-blur-md shadow-2xl rounded-full w-12 h-12 md:w-16 md:h-16 flex items-center justify-center bg-white/10 hover:scale-110 transition-transform duration-300 ">
@@ -93,71 +90,91 @@ const data = await authClient.signIn.social({
               Access your personal archive.
             </p>
           </div>
-          <fieldset>
-            <legend>Name</legend>
-            <input
-              type="text"
-              {...register("name", { required: "Name is required" })}
-              placeholder="Name"
-              className="w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white placeholder:text-white/40 focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 outline-none transition-all"
-            />
-            {errors.name && (
-              <p className="text-red-500">{errors.name.message}</p>
-            )}
-          </fieldset>
-          <fieldset>
-            <legend>Choose your photo</legend>
-            <input
-              type="file"
-              accept="image/*"
-              {...register("photo", { required: "Photo is required" })}
-              placeholder="Choose your photo"
-              className=" w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white placeholder:text-white/40 focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 outline-none transition-all"
-            />
-            {errors.photo && (
-              <p className="text-red-500">{errors.photo.message}</p>
-            )}
-          </fieldset>
-          <fieldset>
-            <legend>Email</legend>
-            <input
-              type="email"
-              {...register("email", { required: " Email is required" })}
-              placeholder="Email"
-              className="w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white placeholder:text-white/40 focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 outline-none transition-all"
-            />
-            {errors.email && (
-              <p className="text-red-500">{errors.email.message}</p>
-            )}
-          </fieldset>
-          <fieldset>
-            <legend>Password</legend>
-            <input
-              type="password"
-              {...register("password", { required: "Password is required" })}
-              placeholder="Password"
-              class="w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white placeholder-white/40 focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 outline-none transition-all"
-            />
-            {errors.password && (
-              <p className="text-red-500">{errors.password.message}</p>
-            )}
-          </fieldset>
-          <button
-            class="bg-gradient-to-br from-[#4f46e5] to-[#7c03d3] shadow-[0_0_30px_-5px_rgba(79,70,229,0.4)] text-white font-semibold py-3 px-6 rounded-lg hover:scale-[1.02] active:scale-[0.98] transition-transform w-full cursor-pointer  duration-500 "
-            type="submit"
+
+          <form
+            className="md:space-y-4 lg:space-y-6"
+            onSubmit={handleSubmit(onSubmit)}
           >
-            Register
-          </button>
-          <h1 className="font-bold text-center border-b border-indigo-500">
-            Or
-          </h1>
-          <button onClick={signWithGoogle} class="bg-gradient-to-br from-[#4f46e5] to-[#7c03d3] shadow-[0_0_30px_-5px_rgba(79,70,229,0.4)] text-white font-semibold py-3 px-6 rounded-lg hover:scale-[1.02] active:scale-[0.98] transition-transform w-full cursor-pointer duration-500">
-            <span className="flex gap-2 items-center justify-center">
-              <FaGoogle />
-              Sign in with Google
-            </span>
-          </button>
-        </form>
+            <fieldset>
+              <legend>Name</legend>
+              <input
+                type="text"
+                {...register("name", { required: "Name is required" })}
+                placeholder="Name"
+                className="w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white placeholder:text-white/40 focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 outline-none transition-all"
+              />
+              {errors.name && (
+                <p className="text-red-500">{errors.name.message}</p>
+              )}
+            </fieldset>
+            <fieldset>
+              <legend>Choose your photo</legend>
+              <input
+                type="file"
+                accept="image/*"
+                {...register("photo", { required: "Photo is required" })}
+                placeholder="Choose your photo"
+                className=" w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white placeholder:text-white/40 focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 outline-none transition-all"
+              />
+              {errors.photo && (
+                <p className="text-red-500">{errors.photo.message}</p>
+              )}
+            </fieldset>
+            <fieldset>
+              <legend>Email</legend>
+              <input
+                type="email"
+                {...register("email", { required: " Email is required" })}
+                placeholder="Email"
+                className="w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white placeholder:text-white/40 focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 outline-none transition-all"
+              />
+              {errors.email && (
+                <p className="text-red-500">{errors.email.message}</p>
+              )}
+            </fieldset>
+            <fieldset>
+              <legend>Password</legend>
+              <input
+                type="password"
+                {...register("password", { required: "Password is required" })}
+                placeholder="Password"
+                class="w-full bg-white/5 border border-white/10 rounded-lg px-4 py-3 text-white placeholder-white/40 focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 outline-none transition-all"
+              />
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
+            </fieldset>
+            <button
+              class="bg-gradient-to-br from-[#4f46e5] to-[#7c03d3] shadow-[0_0_30px_-5px_rgba(79,70,229,0.4)] text-white font-semibold py-3 px-6 rounded-lg hover:scale-[1.02] active:scale-[0.98] transition-transform w-full cursor-pointer  duration-500 "
+              type="submit"
+            >
+              Register
+            </button>
+            <h1 className="font-bold text-center border-b border-indigo-500">
+              Or
+            </h1>
+          </form>
+            <button
+              onClick={signWithGoogle}
+              class="bg-gradient-to-br from-[#4f46e5] to-[#7c03d3] shadow-[0_0_30px_-5px_rgba(79,70,229,0.4)] text-white font-semibold py-3 px-6 rounded-lg hover:scale-[1.02] active:scale-[0.98] transition-transform w-full cursor-pointer duration-500"
+            >
+              <span className="flex gap-2 items-center justify-center">
+                <FaGoogle />
+                Sign in with Google
+              </span>
+            </button>
+            <div className="mt-6 text-center text-xs md:text-sm">
+              <span className="text-gray-400">
+                Already a member of MangoBooks?
+              </span>
+              <Link
+                href="/login"
+                className="ml-2 text-orange-500 hover:text-orange-700 font-bold transition-colors"
+              >
+                Sign In
+              </Link>
+            </div>
+        </div>
       </div>
     </div>
   );
